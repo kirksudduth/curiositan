dcc308b2487e27127e707cbecd706f651062b10a
Kirk Sudduth <kirksudduth@gmail.com>
2020-06-15 10:24:56 -0500

    added a user to the database and started serving it locally

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { Image } from "semantic-ui-react";
+import { Image, Header } from "semantic-ui-react";
 // import keys from "../../keys"
 
 const Login = () => {
-  // const Image = () => {
-  //   <Image src="https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02590/opgs/edr/fcam/FRB_627421745EDR_F0772254FHAZ00302M_.JPG"
-  //   size="medium" />
-  // }
   return (
     <>
-      <div>MARS PIC</div>
-      <Image
-        // className="image"
-        src="https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02590/opgs/edr/fcam/FRB_627421745EDR_F0772254FHAZ00302M_.JPG"
-        size="small"
-        rounded
+      <Header
+        content="Red Planet: A Curiosity"
+        block
+        textAlign="center"
+        as="h2"
       />
     </>
   );
