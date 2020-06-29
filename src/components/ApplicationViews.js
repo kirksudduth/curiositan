b51e7a15411255523eb8f30c475df18e9b2ddd1a
Kirk Sudduth <kirksudduth@gmail.com>
2020-06-28 23:14:05 -0500

    added username to edit modal

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,53 +1,11 @@
 import { Route } from "react-router-dom";
-import React, { useState } from "react";
-import NewUser from "./NewUser";
+import React from "react";
 import PhotoSearchList from "../components/photos/PhotoSearchList";
 import PhotosSaved from "./photos/PhotosSaved";
-import DataManager from "../modules/DataManager";
 
-const ApplicationViews = (props) => {
-  const setUser = props.setUser;
-  const hasUser = props.hasUser;
-  const credentials = props.credentials;
-  const handleLogin = props.handleLogin;
-  const setCredentials = props.setCredentials;
-  // =====================================================
-  // ******  NEW USER STATE STUFF  ******
-
+const ApplicationViews = () => {
   return (
     <>
-      {/* <Route
-        exact
-        path="/new_user"
-        render={(props) => {
-          return (
-            <NewUser
-              handleNewUserFieldChange={handleNewUserFieldChange}
-              credentials={credentials}
-              handleLogin={handleLogin}
-              setUser={setUser}
-              hasUser={hasUser}
-              {...props}
-            />
-          );
-        }}
-      /> */}
-      {/* <Route
-      exact
-      path="/"
-      render={(props) => {
-        return (
-          <Login
-            credentials={credentials}
-            userLogin={userLogin}
-            handleFieldChange={handleFieldChange}
-            setUser={setUser}
-            hasUser={hasUser}
-            {...props}
-          />
-        );
-      }}
-    /> */}
       <Route
         exact
         path="/photos_search"
