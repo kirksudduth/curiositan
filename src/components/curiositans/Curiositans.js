43c5c73b8d4a988ce4dd73097ccc6b40f05d5d97
Kirk Sudduth <kirksudduth@gmail.com>
2020-10-24 22:31:18 -0500

    added all Curiositans to the page -- just need to refine the search

--- src/components/curiositans/Curiositans.js
+++ src/components/curiositans/Curiositans.js
@@ -1,13 +1,34 @@
-import React from "react";
-import { Header } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+
+import { Card, Grid, Header } from "semantic-ui-react";
+import DataManager from "../../modules/DataManager";
 
 const Curiositans = (props) => {
+  const getCuriositans = DataManager.getCuriositans;
+  const [curiositans, setCuriositans] = useState([])
+
+  useEffect(() => {
+    getCuriositans().then((result) => 
+    setCuriositans(result)
+    );
+  }, [])
+
   return (
     <>
       <Header textAlign="center">
-        Not quite there yet! Definitely before the next man-made object gets to
-        Mars.
+        Find a fellow Curiositan!
       </Header>
+      {curiositans.reverse().map((user) => (
+        <Grid.Column key={user.id}>
+          <Card
+          fluid
+          raised
+          key={user.id}
+          className="curiositan_card">
+            <Card.Description>{user.username}</Card.Description>
+          </Card>
+        </Grid.Column>
+      ))}
     </>
   );
 };
