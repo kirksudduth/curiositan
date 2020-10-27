68f64f51438018c220d9a6b7f5c704da98dd26c9
Kirk Sudduth <kirksudduth@gmail.com>
2020-10-27 14:00:01 -0500

    double checked to make sure curiositans are being sent through props

--- src/components/curiositans/Autocomplete.js
+++ src/components/curiositans/Autocomplete.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Input } from 'semantic-ui-react'
 
 const Autocomplete = (props) => {
+
+    // utilize useState() to get the input of the search bar to search
+    // Curiositans.
     const [searchInput, setSearchInput] = useState({input: "",})
     const handleFieldChange = (evt) => {
-        const stateToChange = {...searchInput};
+        const stateToChange = {...searchInput}
         stateToChange.input = evt.target.value;
         setSearchInput(stateToChange);
     }
