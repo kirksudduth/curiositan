92fd881349719b397719540455df2d7bca204262
Kirk Sudduth <kirksudduth@gmail.com>
2020-10-27 14:32:26 -0500

    got the curiositans populating the search

--- src/components/curiositans/Autocomplete.js
+++ src/components/curiositans/Autocomplete.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { Input } from 'semantic-ui-react'
 
 const Autocomplete = (props) => {
-
+    const curiositans = props.curiositans
     // utilize useState() to get the input of the search bar to search
     // Curiositans.
     const [searchInput, setSearchInput] = useState({input: "",})
+    const [display, setDisplay] = useState(false)
     const handleFieldChange = (evt) => {
         const stateToChange = {...searchInput}
         stateToChange.input = evt.target.value;
@@ -13,7 +14,18 @@ const Autocomplete = (props) => {
     }
     return(
         <>
-            <Input type="text" icon="users" iconPosition="left" placeholder="Search Curiositans..." onChange={handleFieldChange} />
+            <Input type="text" icon="users" iconPosition="left" placeholder="Search Curiositans..." onChange={handleFieldChange} onClick={() => setDisplay(!display)} />
+            {display && (
+                <div>
+                    {curiositans.map((c) => {
+                        return (
+                            <div className="option" key={c.id}>
+                            <span>{c.username}</span>
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
         </>
     )
 }
