3c6cab24f5b40dbc96f6cafe9e810b2a77e5b7c1
Kirk Sudduth <kirksudduth@gmail.com>
2020-10-27 22:06:30 -0500

    got my curiositans showing up in search but not getting filtered yet

--- src/components/curiositans/Autocomplete.js
+++ src/components/curiositans/Autocomplete.js
@@ -5,21 +5,25 @@ const Autocomplete = (props) => {
     const curiositans = props.curiositans
     // utilize useState() to get the input of the search bar to search
     // Curiositans.
-    const [searchInput, setSearchInput] = useState({input: "",})
+    const [searchInput, setSearchInput] = useState("")
     const [display, setDisplay] = useState(false)
     const handleFieldChange = (evt) => {
-        const stateToChange = {...searchInput}
-        stateToChange.input = evt.target.value;
+        const stateToChange = searchInput
+        stateToChange = evt.target.value;
         setSearchInput(stateToChange);
+    }
+    const setCuriositan = curiositan => {
+        setSearchInput(curiositan);
+        setDisplay(false);
     }
     return(
         <>
-            <Input type="text" icon="users" iconPosition="left" placeholder="Search Curiositans..." onChange={handleFieldChange} onClick={() => setDisplay(!display)} />
+            <Input value={searchInput} type="text" icon="users" iconPosition="left" placeholder="Search Curiositans..." onChange={(event) => setSearchInput(event.target.value)} onClick={() => setDisplay(!display)} />
             {display && (
                 <div>
-                    {curiositans.map((c) => {
+                    {curiositans.filter(({username}) => username.indexOf(searchInput.toLowerCase()) > -1).map((c, i) => {
                         return (
-                            <div className="option" key={c.id}>
+                            <div onCLick={() => setCuriositan(c.username)} className="option" key={i}>
                             <span>{c.username}</span>
                             </div>
                         )
