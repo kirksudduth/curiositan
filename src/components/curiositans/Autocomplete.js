4bce3c6971210898816fcee837a317fa8d298ae0
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-06 11:45:17 -0600

    grabbed the id from the curiositan object

--- src/components/curiositans/Autocomplete.js
+++ src/components/curiositans/Autocomplete.js
@@ -3,16 +3,24 @@ import { Input } from 'semantic-ui-react'
 
 const Autocomplete = (props) => {
     const curiositans = props.curiositans;
+
     // utilize useState() to get the input of the search bar to search
     // Curiositans.
+
     const [searchInput, setSearchInput] = useState("");
     const [filteredCuriositans, setFilteredCuriositans] = useState([]);
+    
+    // wrapperRef allows user to click outside of 'autoContainer'
+    // div and close filtered results
+
     const wrapperRef = useRef(null);
+    const [curiositanId, setCuriositanId] = useState("")
     
     //  THANK YOU LESSON ON CODING!!
     // https://www.youtube.com/watch?v=vXO5JMiKtM8
     // not completely there yet but definitely helped
     // a TON
+
     const [display, setDisplay] = useState(false);
     const setCuriositan = curiositan => {
         setSearchInput(curiositan);
@@ -53,12 +61,14 @@ const Autocomplete = (props) => {
     return(
         <>
         <div ref={wrapperRef} className="autoContainer">
+        <form>
+        <input type="hidden" id={curiositanId} />
             <Input value={searchInput} type="text" icon="users" iconPosition="left" placeholder="Search Curiositans..." onChange={(event) => searchCuriositan(event)} onClick={() => setDisplay(!display)} />
             {display && (
                 <div>
                     {filteredCuriositans.map((c, i) => {
                         return (
-                            <div onClick={() => setCuriositan(c.username)} className="option" key={i} tabIndex="0">
+                            <div onClick={() => {setCuriositan(c.username); setCuriositanId(c.id)}} className="option" key={i} tabIndex="0">
                             <input type="hidden" id={c.id} />
                             <span>{c.username}</span>
                             </div>
@@ -66,6 +76,7 @@ const Autocomplete = (props) => {
                     })}
                 </div>
             )}
+            </form>
             </div>
         </>
     )
