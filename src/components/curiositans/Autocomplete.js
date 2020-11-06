c7d22cb4c16a8ad6a249d97ad04ebe056f9640be
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-06 11:04:05 -0600

    got it looking better by using flexbox

--- src/components/curiositans/Autocomplete.js
+++ src/components/curiositans/Autocomplete.js
@@ -2,17 +2,18 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Input } from 'semantic-ui-react'
 
 const Autocomplete = (props) => {
-    const curiositans = props.curiositans
+    const curiositans = props.curiositans;
     // utilize useState() to get the input of the search bar to search
     // Curiositans.
     const [searchInput, setSearchInput] = useState("");
     const [filteredCuriositans, setFilteredCuriositans] = useState([]);
+    const wrapperRef = useRef(null);
     
     //  THANK YOU LESSON ON CODING!!
     // https://www.youtube.com/watch?v=vXO5JMiKtM8
     // not completely there yet but definitely helped
     // a TON
-    const [display, setDisplay] = useState(false)
+    const [display, setDisplay] = useState(false);
     const setCuriositan = curiositan => {
         setSearchInput(curiositan);
         setDisplay(false);
@@ -35,14 +36,29 @@ const Autocomplete = (props) => {
         }))
     }, [searchInput, curiositans])
 
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside)
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [])
+
+    const handleClickOutside = event => {
+        const {current: wrap} = wrapperRef;
+        if (wrap && !wrap.contains(event.target))
+            setDisplay(false) 
+    }
+
     return(
         <>
+        <div ref={wrapperRef} className="autoContainer">
             <Input value={searchInput} type="text" icon="users" iconPosition="left" placeholder="Search Curiositans..." onChange={(event) => searchCuriositan(event)} onClick={() => setDisplay(!display)} />
             {display && (
                 <div>
                     {filteredCuriositans.map((c, i) => {
                         return (
-                            <div onClick={() => setCuriositan(c.username)} className="option" key={i}>
+                            <div onClick={() => setCuriositan(c.username)} className="option" key={i} tabIndex="0">
                             <input type="hidden" id={c.id} />
                             <span>{c.username}</span>
                             </div>
@@ -50,6 +66,7 @@ const Autocomplete = (props) => {
                     })}
                 </div>
             )}
+            </div>
         </>
     )
 }
