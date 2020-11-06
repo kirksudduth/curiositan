03466358a439704526241e8e7610260fa82eaed9
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-06 10:31:58 -0600

    got all filtered results appearing as they should

--- src/components/curiositans/Autocomplete.js
+++ src/components/curiositans/Autocomplete.js
@@ -5,7 +5,8 @@ const Autocomplete = (props) => {
     const curiositans = props.curiositans
     // utilize useState() to get the input of the search bar to search
     // Curiositans.
-    const [searchInput, setSearchInput] = useState("")
+    const [searchInput, setSearchInput] = useState("");
+    const [filteredCuriositans, setFilteredCuriositans] = useState([]);
     
     //  THANK YOU LESSON ON CODING!!
     // https://www.youtube.com/watch?v=vXO5JMiKtM8
@@ -20,15 +21,29 @@ const Autocomplete = (props) => {
     const searchCuriositan = event => {
         setSearchInput(event.target.value)
     }
-    console.table("Curiositans:", curiositans)
+
+    // THANK YOU D'Coders!!
+    // https://www.youtube.com/watch?v=Q8JyF3wpsHc
+    // my filtered results weren't all coming back with 
+    // how Lesson was doing it so I found another video
+    // and implemented useEffect and useState to set an new
+    // filteredCuriositans empty array for the search
+
+    useEffect(() => {
+        setFilteredCuriositans(curiositans.filter( curiositan => {
+            return curiositan.username.toLowerCase().includes(searchInput.toLowerCase())
+        }))
+    }, [searchInput, curiositans])
+
     return(
         <>
             <Input value={searchInput} type="text" icon="users" iconPosition="left" placeholder="Search Curiositans..." onChange={(event) => searchCuriositan(event)} onClick={() => setDisplay(!display)} />
             {display && (
                 <div>
-                    {curiositans.filter(({username}) => username.indexOf(searchInput.toLowerCase()) > -1).map((c, i) => {
+                    {filteredCuriositans.map((c, i) => {
                         return (
                             <div onClick={() => setCuriositan(c.username)} className="option" key={i}>
+                            <input type="hidden" id={c.id} />
                             <span>{c.username}</span>
                             </div>
                         )
